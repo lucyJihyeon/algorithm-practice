a692d1d67f72315e92f8e6151f15f51f40d970c8
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-06 00:08:13 -0800

    fizz-buzz(js)

--- sum-array/test-Java/src/main/sumArray.java
+++ sum-array/test-Java/src/main/sumArray.java
@@ -1,5 +1,5 @@
-public class SumArray {
-    public static int sumArray(int[] arr)   {
+public class sumArray {
+    public static int SumArray(int[] arr)   {
         int total = 0;
         for (int i = 0; i < arr.length; i++)    {
             total += arr[i];
--- sum-array/test-Java/test/SumArrayTest.java
+++ sum-array/test-Java/test/SumArrayTest.java
@@ -1,11 +1,11 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
+import org.junit.*;
 public class SumArrayTest {
     @Test
     public void testSumArray() {
         int[] arr = {4, 8, 15, 16, 23, 42};
-        int result = SumArray.sumArray(arr);
+        int result = sumArray.SumArray(arr);
         assertEquals(108, result);
     }
+
+    
 }
