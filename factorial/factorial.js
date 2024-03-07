ea967aa02a3adda0bcb14e2fe8106b326b8925f7
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-06 23:41:17 -0800

    factorial

---
+++ factorial/factorial.js
@@ -0,0 +1,15 @@
+// Write code to create a function that returns the factorial of `num`
+
+var factorial = function(num) {
+ let number = [];
+ let factorial =1;
+    for (let i = 1; i <= num; i ++)   {
+        number.push(i);
+    }
+    console.log(number);
+    for (let i =0; i < number.length; i++)  {
+        factorial *= number[i];
+    }
+    return factorial
+    
+};
