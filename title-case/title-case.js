a0dc6c4f3b495a2e93b6bb35e0f9fdd013da7285
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-07 15:02:38 -0800

    updated title-case

--- title-case/title-case.js
+++ title-case/title-case.js
@@ -1,10 +1,13 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
 var titleCase = function (str) {
-  const newStr = str
-    .split(" ")
-    .map((w) => w[0].toUpperCase() + w.substring(1))
-    .join(" ");
-    return newStr;
+  const newStr = str.split(" ");
+  let words = [];
+  for (let i =0; i < newStr.length; i++)  {
+    const word = newStr[i].substring(0,1).toUpperCase() + newStr[i].substring(1);
+    words.push(word);
+  }
+  return words.join(" ");
 };
 
+//time complexity = O(n)
