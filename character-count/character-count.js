086c3e9f11ca0ec21c5407ec3e77a5165cd8239f
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-08 20:51:52 -0800

    character-count

--- character-count/character-count.js
+++ character-count/character-count.js
@@ -1,18 +1,42 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
+
+//first solution (On^2)
 var characterCount = function (str) {
-  var smStr = str.toLowercase();
-  var char = smStr.split("");
-  var charMap = {};
-  for (var i = 0; i < char.length; i++) {
-    var count = 0;
-    for (var j = 0; j < char.length; j++) {
-      if (char[i] === char[j]) {
-        count += 1;
-      } 
+  const words = str.split("");
+  let characterCount = {};
+  for (let i = 0; i < words.length; i++) {
+    let count = 0;
+   for ( let j = 0; j < words.length; j++) {
+    if (words[i] == words[j]){
+      count++;
     }
-    charMap[char[i]] = count;
+   }
+   characterCount[words[i]] = count;
   }
-  return charMap;
+  console.log(characterCount);
+  return characterCount;
 };
-characterCount("Hello World");
+
+//second solution (On)
+var characterCount = function (str) {
+let characterCount = {};
+const words = str.split("");
+for (let i = 0; i < words.length; i++)  {
+  let char = words[i];
+  if (characterCount[char]){
+    characterCount[char]++;
+  } else {
+    characterCount[char] = 1;
+  }
+}
+return characterCount;
+}
+//clean-up
+function characterCount(str) {
+  let characterCount = {};
+  for (let char of str) {
+    characterCount[char] = (characterCount[char] || 0) + 1;
+  }
+  return characterCount;
+}
