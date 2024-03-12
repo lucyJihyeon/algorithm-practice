9ee98bd2696e0bc4aa563d7ed384e68199862078
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-12 15:26:39 -0700

    comment-element: second and third solutions

--- common-element/common-element.js
+++ common-element/common-element.js
@@ -4,7 +4,7 @@
 // You may not use the `indexOf` or `includes` method
 
 var commonElement = function(arrA, arrB) {
-    // let i = 0; 
+    // let i = 0;
     // let j = 0;
     // while (i <= arrA.length && j <= arrB.length)    {
     //     if (arrA[i] == arrB[j]){
@@ -19,7 +19,35 @@ var commonElement = function(arrA, arrB) {
             let secondElement = arrB[j];
             if (firstElement == secondElement)  {
                 return firstElement
-            } 
+            }
         }
     }
+};
+
+//second solution using Set
+var commonElement = function(arrA, arrB) {
+    let elementsSet = new Set(arrA);
+    for (let element of arrB) {
+        if (elementsSet.has(element)) {
+            return element; // Return the common element as soon as it's found
+        }
+    }
+};
+
+//third solution without using has
+var commonElement = function (arrA, arrB) {
+  let elementTracker = {};
+
+  // Track elements from the first array
+  for (let element of arrA) {
+    elementTracker[element] = true; // Mark each element as encountered
+  }
+  console.log(elementTracker);
+
+  // Iterate through the second array and check for the common element
+  for (let element of arrB) {
+    if (elementTracker[element]) {
+      return element; // Found the common element
+    }
+  }
 };
