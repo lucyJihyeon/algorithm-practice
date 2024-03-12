775cfc648fdca0c460059c3ad04b3a8c16b12574
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-12 14:56:03 -0700

    reverse-in-place:second solution

--- 03-reverse-in-place/reverse-in-place.js
+++ 03-reverse-in-place/reverse-in-place.js
@@ -1,20 +1,34 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
+//first solution 
 const reverseInPlace = (arr) => {
   let i = 0;
   const n = arr.length 
   while (i < n) {
     let tobeAdded = arr[arr.length - 1];
-    console.log(tobeAdded);
     arr.pop();
-    console.log("after pop: ", arr);
     arr.splice(i,0,tobeAdded);
-    console.log("after splice: ", arr);
     i++;
   }
   return arr;
 };
-const arr = [1, 2, 3, 4, 5];
-const result = reverseInPlace(arr);
-console.log(result);
+
+//second solution 
+const reverseInPlace = (arr)  =>  {
+  let left = 0; // Start from the first item
+  let right = arr.length - 1; // Start from the last item
+
+  while (left < right) {
+    // Swap the elements at the left and right indexes
+    let temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+
+    // Move towards the center
+    left++;
+    right--;
+  }
+
+  return arr;
+}
