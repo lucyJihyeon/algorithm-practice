96014c07c6d1d08f485a6d59f8951ddf907a8a5c
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-12 16:04:49 -0700

    permutation-substring second solution using frequency

--- permutation-substring/permutation-substring.js
+++ permutation-substring/permutation-substring.js
@@ -2,14 +2,36 @@
 // Return true if the second string is a substring of any permutation of the first string
 // Else return false
 
-var permutationSubstring = function(str, sub) {
-    const strArr = str.split("");
-    const subArr = sub.split("");
-    let count = 0;
-    for (let i =0; i < subArr.length; i++)  {
-        if (strArr.includes(subArr[i])) {
-            count ++;
+
+// var permutationSubstring = function(str, sub) {
+//     const strArr = str.split("");
+//     const subArr = sub.split("");
+//     let count = 0;
+//     for (let i =0; i < subArr.length; i++)  {
+//         if (strArr.includes(subArr[i])) {
+//             count ++;
+//         }
+//     }
+//     return count == subArr.length
+// };
+
+var permutationSubstring = function(str, sub)   {
+    const strFrequency = {};
+    const subFrequency = {};
+
+    for ( let letter of str) {
+        strFrequency[letter] = (strFrequency[letter] || 0) + 1;
+    }
+    for ( let letter of sub)    {
+        subFrequency[letter] = (subFrequency[letter] || 0) + 1;
+    }
+    console.log("strFrequency: ", strFrequency, "stbFrequency: ", subFrequency);
+    for ([letter, count] of Object.entries(subFrequency))   {
+        if (!strFrequency[letter]){
+            return false;
+        } else if ( strFrequency[letter] < subFrequency[letter])    {
+            return false;
         }
     }
-    return count == subArr.length
-};
+    return true;
+}
